bd79037dabab0e690a097278f867a5f0541a11f8
Thiago Marques Siqueira <thiago@pop-os.localdomain>
2020-05-27 23:22:51 +0100

    Some examples using rest and spread

---
+++ main.js
@@ -0,0 +1,32 @@
+// REST
+
+const arr = [1, 2, 3, 4];
+
+const userDummy = {
+  name: 'Paul',
+  age: 23,
+  company: 'TheCompany'
+};
+
+function sumWithRest(...params) {
+  return params.reduce((total, next) => total + next );
+};
+
+const [ a, b, ...c ] = arr;
+const { name, ...othersElements } = userDummy;
+
+console.log(name, othersElements);
+console.log(a, b, c);
+console.log(sumWithRest(1, 2, 3, 4));
+
+// SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+
+const secondUser = { ...userDummy, name: 'Steve' };
+
+console.log(arr3);
+console.log(secondUser);
